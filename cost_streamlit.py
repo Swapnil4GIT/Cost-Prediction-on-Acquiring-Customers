c4af4050f8af88701b88390db7b5b88e79e76e24
Swapnil4GIT <100533909+Swapnil4GIT@users.noreply.github.com>
2023-03-17 03:12:03 +0530

    Added problem statement and DQA

--- cost_streamlit.py
+++ cost_streamlit.py
@@ -10,26 +10,111 @@ st.write(
     """
 )
 
+st.write(
+    """
+    ***
+    """
+)
+
+st.write(
+    """
+    <p style="font-size: 18px;">
+    About FoodMart
+    </p>
+     """, unsafe_allow_html=True
+)
+
 # About the FoodMart Company
-col1, col2 = st.columns([3,2])
+col1, col2 = st.columns(2, gap='large')
 
 with col1:
-    st.markdown(
-        """
-        <p style="font-size: 18px;">
-        About FoodMart
-        </p>
-        """, unsafe_allow_html=True
-    )
     st.markdown(
         """
         
         <p style="font-size: 15px;">
         Food Mart (CFM) is a chain of convenience stores in the United States. The private company's headquarters are located in Mentor, Ohio and
         there are currently approximately 325 stores located in the United States. Convenient Food Mart operates on the franchise system.
+        </p>
+        """, unsafe_allow_html=True
+    )
+ 
+with col2:
+    st.markdown(
+        """
+        <p style="font-size: 15px;">
         Food Mart was the nation's third-largest chain of convenience stores as of 1988. The NASDAQ exchange dropped Convenient Food Mart the same
         year when the company failed to meet financial reporting requirements. Carden & Cherry advertised Convenient Food Mart with the Ernest
         character in the 1980s
+        </p>
+        """, unsafe_allow_html=True
+    )
+ 
+#Problem statements and value propositions
+st.write("#")
+col1,col2 = st.columns([2,3], gap='large')
+
+with col1:
+    st.markdown(
+        """
+        <p style="font-size: 18px;">
+        Problem Statement
+        </p>
+        """, unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <p style="font-size:15px;">
+        Predict the accurate cost of running media campaign subject to different market conditions based on the historical data.
+        Explore media campaign running cost in advance to acquire new customers and give an edge to the company in making business decisions.
+        </p>
+        """, unsafe_allow_html=True
+    )
+
+#Data description
+
+with col2:
+    st.markdown(
+        """
+        <p style="font-size: 18px;">
+        Data Quality Analysis
+        </p>
+        """, unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <ul>
+            <li>
+                <p style="font-size:15px;">
+                Available data volume of around 60K customers which were acquired in the past. The data contains their acquisition cost too
+                </p>
+            </li>
+            <li>
+                <p style="font-size:15px;">
+                Shape of data is 60428 * 40 i.e. the given dataset has 60428 rows and 40 columns
+                </p>
+            </li>
+            <li>
+                <p style="font-size:15px;">
+                There is no data point missing across all the features from the given dataset
+                </p>
+            </li>
+            <li>
+                <p style="font-size:15px;">
+                Available data types of features from the given dataset - Discrete numeric/non-numeric & Continuous numeric
+                </p>
+            </li>
+            <li>
+                <p style="font-size:15px;">
+                Total marketing cost of acquiring customers is approx. $60 million/year.
+                This is the consolidate customer acquisition cost for all the customers from the dataset.
+                </p>
+            </li>
+            <li>
+                <p style="font-size:15px;">
+                Target variable identified here is the cost of acquiring a customer.​
+               </p>
+            </li>
+        </ul>
         </p>
         """, unsafe_allow_html=True
     )
